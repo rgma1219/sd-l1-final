bea511d97671bf25936d26f174c17282be88fb8f
Gabriel Rey <rgma1219@outlook.com>
2025-09-07 10:19:20 -0300

    Metodos del modelo y controller creados y funcionando. Han pasado los test aunque estos son inconsistentes.

--- src/controllers.ts
+++ src/controllers.ts
@@ -1,6 +1,39 @@
 import { PelisCollection, Peli } from "./models";
 
+type Options = {
+  id?: number;
+  search?: {
+    title?: string;
+    tag?: string;
+  };
+};
+
 class PelisController {
-  constructor() {}
+  model: PelisCollection;
+  constructor() {
+    this.model = new PelisCollection();
+  }
+
+  async get(options?: Options): Promise<Peli[] | []> {
+    //Busca por ID
+    if (options.id) {
+      const peli = await this.model.getById(options.id);
+      return peli ? [peli] : [];
+    }
+    //Busca por search tanto por title y/o por tag
+    if (options.search) {
+      return await this.model.search(options.search);
+    }
+    return await this.model.getAll();
+  }
+
+  async getOne(options: Options): Promise<Peli | undefined> {
+    const pelis = await this.get(options);
+    return pelis[0];
+  }
+
+  async add(peli: Peli) {
+    return await this.model.add(peli);
+  }
 }
 export { PelisController };
--- src/models.ts
+++ src/models.ts
@@ -1,23 +1,61 @@
 import * as jsonfile from "jsonfile";
-// El siguiente import no se usa pero es necesario
 import "./pelis.json";
-// de esta forma Typescript se entera que tiene que incluir
-// el .json y pasarlo a la carpeta /dist
-// si no, solo usandolo desde la libreria jsonfile, no se dá cuenta
 
-// no modificar estas propiedades, agregar todas las que quieras
 class Peli {
   id: number;
   title: string;
   tags: string[];
 }
 
+type SearchOptions = { title?: string; tag?: string };
+
 class PelisCollection {
-  getAll(): Promise<Peli[]> {
-    return jsonfile.readFile("...laRutaDelArchivo").then(() => {
-      // la respuesta de la promesa
-      return [];
+  async getAll(): Promise<Peli[]> {
+    const listaPelis = await jsonfile.readFile("./src/pelis.json");
+    return listaPelis;
+  }
+
+  async add(peli: Peli): Promise<boolean> {
+    const peliExiste = await this.getById(peli.id);
+    if (peliExiste) {
+      return false;
+    } else {
+      const listaPelis = await this.getAll();
+      listaPelis.push(peli);
+      await jsonfile.writeFile("./src/pelis.json", listaPelis);
+      return true;
+    }
+  }
+
+  async getById(id: number): Promise<Peli | null> {
+    const pelis = await this.getAll();
+    const peliEncontrada = pelis.find((p) => p.id === id);
+    return peliEncontrada || null;
+  }
+
+  async search(options: SearchOptions) {
+    const listaPelis = await this.getAll();
+    const listaFiltrada = listaPelis.filter((p) => {
+      let coincide = true; // Asumimos que coincide
+      if (options.tag) {
+        coincide = coincide && p.tags.some((tag) => tag === options.tag);
+      }
+      if (options.title) {
+        coincide =
+          coincide &&
+          p.title.toLowerCase().includes(options.title.toLowerCase());
+      }
+      return coincide; // Devolvemos si coincide con ambas condiciones
     });
+    return listaFiltrada;
   }
 }
 export { PelisCollection, Peli };
+
+//Pruebas en el modelo directamente
+/* const listadoPelis = new PelisCollection();
+async function mostrarPelis() {
+  const pelis = await listadoPelis.getAll();
+  console.log(pelis);
+}
+mostrarPelis(); */
